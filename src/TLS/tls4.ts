d250f8180818fb639125c255855b8cf282826d2c
Guilherme <guilherme0xff@proton.me>
2022-07-27 16:23:47 -0300

    feat: adiciona estrutura básica para enviar e receber comandos

--- src/TLS/tls4.ts
+++ src/TLS/tls4.ts
@@ -1,13 +1,14 @@
-/* eslint-disable new-cap */
 import PromiseSocket from 'promise-socket';
 import net from 'net';
+import ResponseMessage from './responseMessage';
+import ResponseMessageTank from './tank/response';
 
 class Tls {
-  ip: string;
+  readonly ip: string;
 
-  port: number;
+  readonly port: number;
 
-  private promiseSocket?: PromiseSocket<net.Socket>;
+  private readonly promiseSocket: PromiseSocket<net.Socket>;
 
   /**
    *
@@ -17,32 +18,63 @@ class Tls {
   constructor(ip: string, port: number = 10001) {
     this.ip = ip;
     this.port = port;
+    this.promiseSocket = new PromiseSocket(new net.Socket());
+  }
+
+  private async readAll(): Promise<Buffer> {
+    const result = Buffer.from(
+      await new Promise((resolve) => {
+        let data = Buffer.from([]);
+
+        this.promiseSocket.stream.on('data', (chunk) => {
+          data = Buffer.concat([data, chunk]);
+
+          // ETX (ascii)
+          if (data[data.length - 1] === 0x3) {
+            resolve(data);
+          }
+        });
+      })
+    );
+
+    return result;
   }
 
   async connect() {
-    this.promiseSocket = new PromiseSocket(new net.Socket());
-
     try {
       await this.promiseSocket.connect({
         host: this.ip,
-        port: this.port,
+        port: this.port
       });
-      console.log(`Conectado a ${this.ip}:${this.port}.`);
+      this.promiseSocket.setTimeout(3000);
+
+      console.info(`Conectado a ${this.ip}:${this.port}.`);
     } catch (error: any) {
       console.error('Ocorreu um erro ao se conectar ao TLS4.', error.name);
       throw error;
     }
   }
 
-  async getTanks() {
-    const buffer = Buffer.concat([Buffer.from([0x1]), Buffer.from('i20100')]);
+  /**
+   * Consulta informações do(s) tanque(s).
+   * @param tank Número do tanque a ser buscado. (Todos tanques por padrão)
+   * @returns
+   */
+  async getTanks(tank: string = '00'): Promise<ResponseMessage> {
+    const padTank = tank.padStart(2, '0');
+    const command = `i201${padTank}`;
+    const buffer = Buffer.concat([Buffer.from([0x1]), Buffer.from(command)]);
 
     await this.promiseSocket.write(buffer);
-    await this.promiseSocket.end();
+    const result = await this.readAll();
 
-    const result = this.promiseSocket.readAll();
+    if (result.length === 0) {
+      throw Error('Retorno vazio ao buscar tanques.');
+    }
 
-    return result;
+    const response = new ResponseMessageTank(result);
+
+    return response;
   }
 }
 
