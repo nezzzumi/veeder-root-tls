1876b34aa57241c990d4c8e2d553c2356af140db
Guilherme <guilherme0xff@proton.me>
2022-07-27 17:58:34 -0300

    feat: adiciona método estático `fromBytes`

--- src/TLS/tank/tank.ts
+++ src/TLS/tank/tank.ts
@@ -50,8 +50,45 @@ class Tank {
   }
 
   static fromBytes(buffer: Buffer): Tank {
-    const tankNumber = buffer.subarray(0, 2).toString();
-    console.log(tankNumber);
+    const tankNumber = parseInt(buffer.subarray(0, 2).toString(), 10);
+    const productCode = buffer.subarray(2, 3).toString();
+    const status = buffer.subarray(3, 7).toString();
+    const dataFieldsLength = Number(buffer.subarray(7, 9).toString());
+
+    const parts: Buffer[] = [];
+    const BYTES_DATA_FIELD = 8;
+
+    for (let i = 9; i < buffer.length; i += BYTES_DATA_FIELD) {
+      parts.push(
+        Buffer.from(buffer.subarray(i, i + BYTES_DATA_FIELD).toString(), 'hex')
+      );
+    }
+
+    const volume = parts[0].readFloatBE();
+    const tcVolume = parts[1].readFloatBE();
+    const ullage = parts[2].readFloatBE();
+    const height = parts[3].readFloatBE();
+    const water = parts[4].readFloatBE();
+    const temperature = parts[5].readFloatBE();
+    const waterVolume = parts[6].readFloatBE();
+
+    const tank = new Tank(
+      tankNumber,
+      productCode,
+      new TankStatus(status[0] === '1', status[1] === '1', status[2] === '1'),
+      dataFieldsLength,
+      volume,
+      tcVolume,
+      ullage,
+      height,
+      water,
+      temperature,
+      waterVolume
+    );
+
+    return tank;
+
+    // return tank;
   }
 }
 
