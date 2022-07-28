a57151fffa7eefe8b3be316cc02facc80d2c7716
Guilherme <guilherme0xff@proton.me>
2022-07-28 11:06:12 -0300

    feat: adiciona parsing dos bytes

--- src/TLS/tank/response.ts
+++ src/TLS/tank/response.ts
@@ -4,15 +4,29 @@ import Tank from './tank';
 class ResponseMessageTank extends ResponseMessage {
   readonly tanks: Tank[];
 
+  static tankBytesLength: number = 65;
+
   constructor(buffer: Buffer) {
     super(buffer);
     this.tanks = [];
 
-    // console.log(
-    //   buffer
-    //     .subarray(this.paddingHeader, buffer.indexOf(this.terminationFlag))
-    //     .toString()
-    // );
+    const bytesTanks = buffer.subarray(
+      this.paddingHeader,
+      buffer.indexOf(ResponseMessage.terminationFlag)
+    );
+
+    for (
+      let i = 0;
+      i < bytesTanks.length / ResponseMessageTank.tankBytesLength;
+      i += 1
+    ) {
+      const tankBytes = bytesTanks.subarray(
+        i * ResponseMessageTank.tankBytesLength,
+        i * ResponseMessageTank.tankBytesLength +
+          ResponseMessageTank.tankBytesLength
+      );
+      this.tanks.push(Tank.fromBytes(tankBytes));
+    }
   }
 }
 
