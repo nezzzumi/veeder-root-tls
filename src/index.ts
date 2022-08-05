759026a8ca1026f8e84e21167b23d6a3d4652d02
Guilherme <guilherme0xff@proton.me>
2022-08-05 12:59:40 -0300

    improvement: utiliza ip e porta passados por argumento

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import Tls from './tls/tls4';
 
-const tlsIp = process.env.TLS_IP;
+const tlsIp = process.argv[2];
+const tlsPort = parseInt(process.argv[3], 10) || 10001;
 
 if (!tlsIp) {
   console.error(`Variável de ambiente TLS_IP não definida.`);
   process.exit(1);
 }
 
-const tls = new Tls(tlsIp);
+const tls = new Tls(tlsIp, tlsPort);
 
 (async () => {
   await tls.connect();
