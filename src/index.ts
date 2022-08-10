7fa5dac75160226eafbc476810e471a280ca5870
Guilherme <guilherme0xff@proton.me>
2022-08-10 10:41:22 -0300

    refactor: adiciona export do Tls

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,3 @@
-import Tank from './tls/tank/tank';
 import Tls from './tls/tls4';
 
-const tlsIp = process.argv[2];
-const tlsPort = parseInt(process.argv[3], 10) || 10001;
-
-if (!tlsIp) {
-  console.error(`IP não informado.`);
-  process.exit(1);
-}
-
-const tls = new Tls(tlsIp, tlsPort);
-
-(async () => {
-  try {
-    await tls.connect();
-  } catch (error) {
-    tls.destroy();
-    return;
-  }
-
-  const responseTanks = await tls.getTanks();
-
-  responseTanks.tanks.forEach((tank: Tank) => {
-    console.log(`T${tank.id} -> ${tank.volume}`);
-  });
-
-  tls.destroy();
-})();
+export default Tls;
